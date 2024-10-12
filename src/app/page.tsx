d1dc2bf13d04de73507b872b53f933d83411bb1a
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-10-12 18:22:49 -0500

    :tada: First commit.

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { width } from 'tailwindcss/defaultTheme'
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center px-2 py-10">
+      <h1 className="text-center text-4xl font-light">This website is under construction</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/theme'
+import typography from '@tailwindcss/typography'
+import colors from 'tailwindcss/colors'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        'DEFAULT': '1rem',
+        'sm': '2rem',
+        'lg': '4rem',
+        'xl': '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors: {
+        gray: colors.neutral,
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', ...defaultTheme.fontFamily.sans],
+        mono: ['var(--font-geist-mono)', ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [
+    typography,
+    nextui({}),
+  ],
+}
+export default config
