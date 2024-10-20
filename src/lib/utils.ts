5948fdfda73177baa2fb5dfe2164cf111f07383f
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-10-19 20:08:19 -0500

    :sparkles: Add a function that allows users to copy code blocks to the clipboard.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,16 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function copyToClipboard(text: string) {
+  try {
+    await navigator.clipboard.writeText(text)
+  }
+  catch (error) {
+    console.error(error)
+
+    return false
+  }
+
+  return true
+}
