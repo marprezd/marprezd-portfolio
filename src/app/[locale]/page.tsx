d15d91fd06a3658f478085f2f71eef923a7708c1
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-10-30 15:03:57 -0500

    :sparkles: Add Hero components.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,24 @@
-import { useTranslations } from 'next-intl'
+import dynamic from 'next/dynamic'
+import React from 'react'
+
+function Loading() {
+  return null
+}
+
+const HeroComponent = dynamic(
+  () => import('../../components/hero'),
+  {
+    loading: () => <Loading />,
+  },
+)
 
 export default function Home() {
-  const t = useTranslations('site')
-
   return (
-    <div className="flex items-center justify-center px-2 py-10">
-      <h1 className="text-center text-4xl font-light">{t('under-construction')}</h1>
-    </div>
+    <main className="relative pb-20">
+      {/* Hero Section */}
+      <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <HeroComponent />
+      </section>
+    </main>
   )
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,83 @@
+import { Link } from '@nextui-org/link'
+import { button as buttonStyles } from '@nextui-org/theme'
+import { IconArrowNarrowRight, IconBrandGithub } from '@tabler/icons-react'
+import { useTranslations } from 'next-intl'
+import React from 'react'
+
+export default function Hero() {
+  const t = useTranslations('site')
+  const githubUrl = 'https://www.github.com/marprezd'
+  const substackUrl = 'https://marprezd.substack.com/?r=3lz4aj&utm_campaign=pub-share-checklist'
+
+  return (
+    <div>
+      <header className="flex justify-center">
+        {/* Link to my Substack profile */}
+        <Link
+          isExternal
+          href={substackUrl}
+          className="inline-flex items-center gap-x-2 rounded-full border border-neutral-200 bg-white p-1 ps-3 text-sm text-neutral-800 transition hover:border-neutral-300 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:border-neutral-600 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-neutral-600"
+        >
+          {t('hero.substack-profile')}
+          <span className="inline-flex items-center justify-center gap-x-2 rounded-full bg-neutral-200 px-2.5 py-1.5 text-sm font-semibold text-neutral-600 dark:bg-neutral-700 dark:text-neutral-400">
+            <svg
+              className="size-4 shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </span>
+        </Link>
+      </header>
+      <div className="mx-auto mt-5 max-w-4xl space-y-4 text-center">
+        {/* Title and description */}
+        {t.rich('hero.title', {
+          h1: chunks => (
+            <h1 className="block text-4xl font-bold tracking-tight text-default-800 md:text-5xl lg:text-6xl">{chunks}</h1>
+          ),
+          span: chunks => (
+            <span className="bg-gradient-to-r from-cyan-400 to-emerald-600 bg-clip-text text-transparent">{chunks}</span>
+          ),
+        })}
+        <p className="text-lg text-default-700 md:text-xl">
+          {t('hero.description')}
+        </p>
+      </div>
+      <footer className="mt-8 flex justify-center gap-3">
+        {/* CTA Link: It all starts here! */}
+        <Link
+          className={buttonStyles({
+            color: 'primary',
+            radius: 'full',
+            variant: 'shadow',
+          })}
+          href="/blog"
+        >
+          {t('hero.started')}
+          <IconArrowNarrowRight size={20} />
+        </Link>
+        {/* Link to my Github profile */}
+        <Link
+          isExternal
+          className={buttonStyles({
+            color: 'secondary',
+            variant: 'bordered',
+            radius: 'full',
+          })}
+          href={githubUrl}
+        >
+          <IconBrandGithub size={20} />
+          GitHub
+        </Link>
+      </footer>
+    </div>
+  )
+}
