11e5b1613c3a0a7be4907d16a85c2c0364ca249c
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-11-04 16:18:20 -0500

    :art: Update homepage.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -26,16 +26,34 @@ const FeaturedPostsComponent = dynamic(
   },
 )
 
+const StatsComponent = dynamic(
+  () => import('../../components/website-stats'),
+  {
+    loading: () => <Loading />,
+  },
+)
+
 export default function Home({ params: { locale } }: HomeProps) {
   return (
-    <main className="relative space-y-10">
+    <main className="relative">
       {/* Hero Section */}
-      <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <section className="mx-auto max-w-7xl px-4 pb-10 sm:px-6 md:pb-20 lg:px-8">
         <HeroComponent />
       </section>
-      <section className="mx-auto max-w-5xl px-4 py-10 sm:px-6 md:py-20 lg:px-8">
-        <div className="space-y-4 px-4 sm:space-y-8 md:space-y-10 lg:px-10">
-          <FeaturedPostsComponent
+      <section className="bg-white dark:bg-gray-900">
+        <div className="mx-auto max-w-5xl px-4 py-10 sm:px-6 md:py-20 lg:px-8">
+          <div className="space-y-4 px-4 sm:space-y-8 md:space-y-10 lg:px-10">
+            <FeaturedPostsComponent
+              params={{
+                locale,
+              }}
+            />
+          </div>
+        </div>
+      </section>
+      <section className="bg-gradient-to-bl from-cyan-900 via-cyan-950 to-cyan-900">
+        <div className="mx-auto max-w-5xl px-4 py-10 sm:px-6 md:py-20 lg:px-8">
+          <StatsComponent
             params={{
               locale,
             }}
