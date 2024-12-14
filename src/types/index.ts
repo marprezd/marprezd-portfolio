8809f333eb51809fdf2919c5f4c2281c746df8a9
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2024-12-14 14:56:33 -0500

    Add Wakapi and Github stats types.

--- src/types/index.ts
+++ src/types/index.ts
@@ -170,3 +170,63 @@ export type CompositeTypes<
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema['CompositeTypes']
     ? PublicSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
     : never
+
+// wakapi and github stats
+export interface WakapiSummaries {
+  data: {
+    data: [
+      {
+        grand_total: {
+          hours: number
+        }
+        range: {
+          start: string
+        }
+      },
+    ]
+    daily_average: {
+      seconds: number
+    }
+    end: string
+  }
+}
+
+export interface WakapiStats {
+  data: {
+    data: {
+      total_seconds: number
+      daily_average: number
+      languages: [
+        {
+          name: string
+          total_seconds: number
+        },
+      ]
+      categories: [
+        {
+          name: string
+          total_seconds: number
+        },
+      ]
+    }
+  }
+}
+
+export interface Stats {
+  stars: number
+  totalCommits: number
+  totalRepos: number
+  followers: number
+  contributions: number
+  prs: number
+  issues: number
+  contributionCalendar: {
+    totalContributions: string
+    weeks: [{
+      contributionDays: [{
+        date: string
+        contributionCount: number
+      }]
+    }]
+  }
+}
