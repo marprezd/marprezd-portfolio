97004765c9aba90d4ac6023febcdf9e02b86a358
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2025-02-12 11:18:34 -0500

    Update tailwind config and globals styles.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/content/**/*.mdx',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -59,13 +58,6 @@ export default {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
-        chart: {
-          1: 'hsl(var(--chart-1))',
-          2: 'hsl(var(--chart-2))',
-          3: 'hsl(var(--chart-3))',
-          4: 'hsl(var(--chart-4))',
-          5: 'hsl(var(--chart-5))',
-        },
         sidebar: {
           'DEFAULT': 'hsl(var(--sidebar-background))',
           'foreground': 'hsl(var(--sidebar-foreground))',
