671cbfa7be20de5a5c1c7efd77bf0fd548b6722a
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2025-02-16 16:11:42 -0500

    Update DefaultHero component.

--- src/components/default-hero.tsx
+++ src/components/default-hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CldImage from './cld-image'
 
 interface Props {
   title: string
@@ -7,18 +8,37 @@ interface Props {
 
 export default function DefaultHero({ title, description }: Props) {
   return (
-    <div className="flex flex-col space-y-4 py-10">
-      <h1 className="block text-4xl font-bold md:text-5xl">
-        {title}
-      </h1>
-      <div>
-        <p className="text-gray-700 dark:text-gray-300">
-          {description}
-        </p>
-        <div className="text-left">
-          <span className="inline-block h-1 w-40 rounded-full bg-red-500 dark:bg-red-400" />
-          <span className="ml-1 inline-block h-1 w-3 rounded-full bg-red-500 dark:bg-red-400" />
-          <span className="ml-1 inline-block size-1 rounded-full bg-red-500 dark:bg-red-400" />
+    <div className="grid grid-cols-12">
+      <div className="col-span-full lg:col-span-6 lg:place-self-center">
+        <h1 className="mb-4 text-center text-4xl font-bold md:text-[2.6rem] lg:text-left">
+          {title}
+        </h1>
+        <div>
+          <p className="text-center text-gray-700 dark:text-gray-300 lg:text-left">
+            {description}
+          </p>
+          <div className="text-center lg:text-left">
+            <span className="inline-block h-1 w-40 rounded-full bg-red-500 dark:bg-red-400" />
+            <span className="ml-1 inline-block h-1 w-3 rounded-full bg-red-500 dark:bg-red-400" />
+            <span className="ml-1 inline-block size-1 rounded-full bg-red-500 dark:bg-red-400" />
+          </div>
+        </div>
+      </div>
+      <div className="col-span-full lg:col-span-6">
+        <div className="flex w-auto max-w-md">
+          <CldImage
+            alt="Programmer Working"
+            width={495}
+            height={405}
+            sizes="100vw"
+            src="marprez-dev/hero/programmer-working_pvz8mu.png"
+            crop={{
+              width: 495,
+              height: 405,
+              type: 'fill',
+            }}
+            quality={75}
+          />
         </div>
       </div>
     </div>
