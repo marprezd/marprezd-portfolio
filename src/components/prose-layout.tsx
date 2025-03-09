91dbb5099ff257d434ed063869a5337aca08a4e6
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2025-03-09 16:31:43 -0500

    Add prose-layout component.

---
+++ src/components/prose-layout.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils'
+
+function ProseLayout({
+  children,
+  className,
+  ...restProps
+}: React.ComponentProps<'div'>) {
+  return (
+    <div
+      className={cn('prose prose-slate dark:prose-invert prose-headings:font-bold prose-code:before:hidden prose-code:after:hidden', className)}
+      {...restProps}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default ProseLayout
